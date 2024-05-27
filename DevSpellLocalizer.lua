3abc303882893f3f4ac7000d82e12c8222ee38ee
enderneko <enderneko-dev@outlook.com>
2024-05-27 15:38:46 +0800

    Update spell localizer

--- DevSpellLocalizer.lua
+++ DevSpellLocalizer.lua
@@ -14,7 +14,10 @@ scroll.eb:SetScript("OnEditFocusGained", function()
     scroll.eb:HighlightText()
 end)
 
-local localizeBtn = Dev:CreateButton(spellLocalizerFrame, "Localize", "blue", {350, 20})
+---------------------------------------------------------------------
+-- normal
+---------------------------------------------------------------------
+local localizeBtn = Dev:CreateButton(spellLocalizerFrame, "Normal", "blue", {175, 20})
 localizeBtn:SetPoint("BOTTOMLEFT", spellLocalizerFrame, "TOPLEFT", 0, -1)
 
 localizeBtn:RegisterForDrag("LeftButton")
@@ -78,8 +81,77 @@ localizeBtn:SetScript("OnClick", function()
     scroll.eb:HighlightText()
 end)
 
+---------------------------------------------------------------------
+-- key
+---------------------------------------------------------------------
+local localizeBtn2 = Dev:CreateButton(spellLocalizerFrame, "Key", "blue", {175, 20})
+localizeBtn2:SetPoint("BOTTOMLEFT", localizeBtn, "BOTTOMRIGHT", -1, 0)
+
+localizeBtn2:RegisterForDrag("LeftButton")
+localizeBtn2:SetClampedToScreen(true)
+localizeBtn2:SetScript("OnDragStart", function()
+    spellLocalizerFrame:StartMoving()
+end)
+localizeBtn2:SetScript("OnDragStop", function()
+    spellLocalizerFrame:StopMovingOrSizing()
+end)
+
+localizeBtn2:SetScript("OnClick", function()
+    local text = scroll.eb:GetText()
+    if text == "" then return end
+
+    scroll.eb:SetText("")
+
+    local t = {strsplit("\n", text)}
+
+    local n = 0
+
+    for i, line in pairs(t) do
+        line = strtrim(line)
+
+        local match = line:match("%[%d+%].+")
+
+        if match then
+            local oldLine = match
+            oldLine = oldLine:gsub("%-%-.+$", "")
+            oldLine = strtrim(oldLine)
+
+            line = line:match("%[(%d+)%].+")
+
+            local id = tonumber(line)
+            if id then
+                local name = GetSpellInfo(id)
+                if name then
+                    line = oldLine.." -- "..name
+                else
+                    line = nil
+                end
+            else
+                line = nil
+            end
+        end
+
+        t[i] = line
+
+        if line then
+            n = n + 1
+            if n == 1 then
+                scroll.eb:Insert(line)
+            else
+                scroll.eb:Insert("\n"..line)
+            end
+        end
+    end
+    -- texplore(t)
+    scroll.eb:SetFocus(true)
+    scroll.eb:HighlightText()
+end)
+
+---------------------------------------------------------------------
+-- close
+---------------------------------------------------------------------
 local closeBtn = Dev:CreateButton(spellLocalizerFrame, "Close", "red", {50, 20})
-closeBtn:SetPoint("BOTTOMLEFT", localizeBtn, "BOTTOMRIGHT", -1, 0)
+closeBtn:SetPoint("BOTTOMLEFT", localizeBtn2, "BOTTOMRIGHT", -1, 0)
 closeBtn:SetPoint("BOTTOMRIGHT", spellLocalizerFrame, "TOPRIGHT", 0, -1)
 closeBtn:SetScript("OnClick", function()
     spellLocalizerFrame:Hide()
